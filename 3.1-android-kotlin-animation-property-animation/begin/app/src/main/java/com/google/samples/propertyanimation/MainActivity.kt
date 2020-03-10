c2d2755a62c2ed19b4f843ef549e9a122784974c
nodormu <60243248+nodormu@users.noreply.github.com>
2020-03-10 16:44:20 -0500

    Add files via upload

---
+++ 3.1-android-kotlin-animation-property-animation/begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.propertyanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var star: ImageView
+    private lateinit var rotateButton: Button
+    private lateinit var translateButton: Button
+    private lateinit var scaleButton: Button
+    private lateinit var fadeButton: Button
+    private lateinit var colorizeButton: Button
+    private lateinit var showerButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        star = findViewById(R.id.star)
+        rotateButton = findViewById<Button>(R.id.rotateButton)
+        translateButton = findViewById<Button>(R.id.translateButton)
+        scaleButton = findViewById<Button>(R.id.scaleButton)
+        fadeButton = findViewById<Button>(R.id.fadeButton)
+        colorizeButton = findViewById<Button>(R.id.colorizeButton)
+        showerButton = findViewById<Button>(R.id.showerButton)
+
+        rotateButton.setOnClickListener {
+            rotater()
+        }
+
+        translateButton.setOnClickListener {
+            translater()
+        }
+
+        scaleButton.setOnClickListener {
+            scaler()
+        }
+
+        fadeButton.setOnClickListener {
+            fader()
+        }
+
+        colorizeButton.setOnClickListener {
+            colorizer()
+        }
+
+        showerButton.setOnClickListener {
+            shower()
+        }
+    }
+
+    private fun rotater() {
+    }
+
+    private fun translater() {
+    }
+
+    private fun scaler() {
+    }
+
+    private fun fader() {
+    }
+
+    private fun colorizer() {
+    }
+
+    private fun shower() {
+    }
+
+}
